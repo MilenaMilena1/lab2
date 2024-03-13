c021a804f2f11e6b164253e04338de61dae22823
MilenaMilena1 <128045179+MilenaMilena1@users.noreply.github.com>
2024-03-13 21:56:16 +0300

    Update matrix.h

--- lab2/matrix.h
+++ lab2/matrix.h
@@ -1,1 +1,34 @@
+#ifndef MATRIX_H
+#define MATRIX_H
 
+#include <iostream>
+
+class Matrix {
+private:
+    unsigned int m;
+    unsigned int n;
+    int** data;
+
+public:
+    Matrix(unsigned int rows, unsigned int cols);
+    ~Matrix();
+
+    void fillRandom();
+
+    int* operator[](unsigned int index) const;
+    int& operator()(unsigned int row, unsigned int col);
+
+    Matrix operator+(const Matrix& other) const;
+    Matrix operator-(const Matrix& other) const;
+    Matrix operator*(const Matrix& other) const;
+
+    Matrix& operator+=(const Matrix& other);
+    Matrix& operator-=(const Matrix& other);
+
+    bool operator==(const Matrix& other) const;
+    bool operator!=(const Matrix& other) const;
+
+    friend std::ostream& operator<<(std::ostream& out, const Matrix& matrix);
+};
+
+#endif // MATRIX_H
